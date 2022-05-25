212effdc3be59d0451ad2256f777b7d8b975744a
Fernando747 <fernandojcr5@gmail.com>
2022-05-25 02:59:52 -0500

    Waterjet change

--- CityWash/Assets/_F747/_Code/Objects/Dirt.cs
+++ CityWash/Assets/_F747/_Code/Objects/Dirt.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Dirt : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField] private bool _shrinkOnCleaned;
+
     [Header("Debugging")]
     [SerializeField] private Logger _logger;
 
 
     public void CleanDirt()
     {
-            Log("I got cleaned! " + this.gameObject.name);
-            iTween.ScaleTo(this.gameObject, iTween.Hash("scale",Vector3.zero, "time", 3f, "oncomplete","DestroyObject"));
+        Log("I got cleaned! " + this.gameObject.name);
+        if (_shrinkOnCleaned) iTween.ScaleTo(this.gameObject, iTween.Hash("scale", Vector3.zero, "time", 3f, "oncomplete", "DestroyObject"));
+        else DestroyObject();
     }
 
     private void DestroyObject()
